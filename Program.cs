b00d968c0c2364453663439ea0fc6624938115c9
KachalkinDmitriy <159521740+KachalkinDmitriy@users.noreply.github.com>
2024-04-12 12:01:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿//using System;
+
+//class Program
+//{
+//    struct Jump
+//    {
+//        private string second_name;
+//        private double first_try;
+//        private double second_try;
+//        private double best_try;
+
+//        public Jump(string second_name, double first_try, double second_try)
+//        {
+//            this.second_name = second_name;
+//            this.first_try = first_try;
+//            this.second_try = second_try;
+//            if (first_try > second_try)
+//            {
+//                this.best_try = first_try;
+//            }
+//            else
+//            {
+//                this.best_try = second_try;
+//            }
+//        }
+
+//        public string GetRaceInfo(int place)
+//        {
+//            return $"Место {place} | Фамилия {second_name} | Результат: {best_try} см \n";
+//        }
+
+//        public double GetResult()
+//        {
+//            return best_try;
+//        }
+//    }
+
+//    static void Main()
+//    {
+//        Jump[] jumping = new Jump[5];
+
+//        jumping[0] = new Jump("Иванов", 140, 150);
+//        jumping[1] = new Jump("Петров", 155, 139);
+//        jumping[2] = new Jump("Сидоров", 149, 151);
+//        jumping[3] = new Jump("Кузнецов", 170, 160);
+//        jumping[4] = new Jump("Макаров", 150, 157);
+
+//        for (int i = 1; i < jumping.Length; i++)
+//        {
+//            Jump key = jumping[i];
+//            int j = i - 1;
+
+//            while (j >= 0 && jumping[j].GetResult() < key.GetResult())
+//            {
+//                jumping[j + 1] = jumping[j];
+//                j--;
+//            }
+
+//            jumping[j + 1] = key;
+//        }
+
+//        for (int i = 0; i < jumping.Length; i++)
+//        {
+//            Console.Write(jumping[i].GetRaceInfo(i + 1));
+//        }
+//    }
+//}
+
+
---
+++ Program2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Program
+{
+    struct Exam
+    {
+        private string second_name;
+        private double[] exams;
+        private double points;
+
+        public Exam(string second_name, double[] exams)
+        {
+            this = new Exam();
+            this.second_name = second_name;
+            this.exams = exams;
+            CalculatePoints();
+        }
+
+        private void CalculatePoints()
+        {
+            double sum = 0;
+            foreach (double exam in exams)
+            {
+                sum += exam;
+            }
+            points = sum / exams.Length;
+        }
+
+        public string GetExamInfo()
+        {
+            if (points >= 4)
+            {
+                return $"Фамилия {second_name} | Средний балл: {points}\n";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public double GetResult()
+        {
+            return points;
+        }
+    }
+
+    static void Main()
+    {
+        Exam[] students = new Exam[5];
+
+        students[0] = new Exam("Иванов", new double[] { 5, 4, 3, 5 });
+        students[1] = new Exam("Петров", new double[] { 5, 3, 4, 4 });
+        students[2] = new Exam("Сидоров", new double[] { 3, 3, 3, 4 });
+        students[3] = new Exam("Кузнецов", new double[] { 5, 5, 4, 5 });
+        students[4] = new Exam("Макаров", new double[] { 5, 4, 3, 2 });
+
+        for (int i = 0; i < students.Length - 1; i++)
+        {
+            for (int j = 0; j < students.Length - 1 - i; j++)
+            {
+                if (students[j].GetResult() < students[j + 1].GetResult())
+                {
+                    Exam temp = students[j];
+                    students[j] = students[j + 1];
+                    students[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.Write(students[i].GetExamInfo());
+        }
+    }
+}
+
