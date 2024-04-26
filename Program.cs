190a622d8c46747a91bb4b131604556a187b16d8
KachalkinDmitriy <159521740+KachalkinDmitriy@users.noreply.github.com>
2024-04-26 12:26:01 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,69 +1,129 @@
-﻿//using System;
+﻿
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
-//class Program
-//{
-//    struct Jump
-//    {
-//        private string second_name;
-//        private double first_try;
-//        private double second_try;
-//        private double best_try;
+abstract class Task
+{
+    protected string text;
+    public string Text
+    {
+        get => text;
+        protected set => text = value;
+    }
+    public Task(string text)
+    {
+        this.text = text;
+    }
+}
 
-//        public Jump(string second_name, double first_try, double second_try)
-//        {
-//            this.second_name = second_name;
-//            this.first_try = first_try;
-//            this.second_try = second_try;
-//            if (first_try > second_try)
-//            {
-//                this.best_try = first_try;
-//            }
-//            else
-//            {
-//                this.best_try = second_try;
-//            }
-//        }
+class Task1 : Task
+{
+    [JsonConstructor]
+    public Task1(string text) : base(text) { }
+    public override string ToString()
+    {
+        return ($"\n  Последние слова предложений:\n {LastWords()} \n");
+    }
 
-//        public string GetRaceInfo(int place)
-//        {
-//            return $"Место {place} | Фамилия {second_name} | Результат: {best_try} см \n";
-//        }
+    private string LastWords()
+    {
+        string[] words = text.Split(new char[] { ' ', ',', '.', ';', '!', '?', ':', '/' });
+        string[] LastWord;
 
-//        public double GetResult()
-//        {
-//            return best_try;
-//        }
-//    }
+        for (int i = 0; i < words.Length; i++)
+        {
+            //я пытался
+        }
+    }
+}
+class Task2 : Task
+{
+    [JsonConstructor]
+    public Task2(string text) : base(text) { }
+    public override string ToString()
+    {
+        return $"\n Измененный текст:\n {UpperLower()}\n";
+    }
 
-//    static void Main()
-//    {
-//        Jump[] jumping = new Jump[5];
+    private string UpperLower()
+    {
+        string[] words = text.Split(new char[] { '/', ',', '.', '!', '?', ' ', ';', ':'});
 
-//        jumping[0] = new Jump("Иванов", 140, 150);
-//        jumping[1] = new Jump("Петров", 155, 139);
-//        jumping[2] = new Jump("Сидоров", 149, 151);
-//        jumping[3] = new Jump("Кузнецов", 170, 160);
-//        jumping[4] = new Jump("Макаров", 150, 157);
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                words[i] = words[i].ToUpper();
+            }
+            else
+            {
+                words[i] = words[i].ToLower();
+            }
+        }
+        return text;
+    }
+}
+class Json
+{
+    public static void Write<T>(T obj, string filepath)
+    {
+        using (FileStream fs = new FileStream(filepath, FileMode.OpenOrCreate))
+        {
+            JsonSerializer.Serialize(fs, obj);
+        }
+    }
+    public static T Read<T>(string filepath)
+    {
+        using (FileStream fs = new FileStream(filepath, FileMode.OpenOrCreate))
+        {
+            return JsonSerializer.Deserialize<T>(fs);
+        }
+        return default(T);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        string text = "На вход подается текст. На выход - массив строк. Выписать последние слова всех преложений.";
+        Task[] tasks = { new Task1(text), new Task2(text) };
+        Console.WriteLine(tasks[0]);
+        Console.WriteLine(tasks[1]);
 
-//        for (int i = 1; i < jumping.Length; i++)
-//        {
-//            Jump key = jumping[i];
-//            int j = i - 1;
+        string path = @"C:\Users\m2302100\Desktop";
+        string folder_name = "Solution";
+        path = Path.Combine(path, folder_name);
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        string file_name_1 = "task_1.json";
+        string file_name_2 = "task_2.json";
 
-//            while (j >= 0 && jumping[j].GetResult() < key.GetResult())
-//            {
-//                jumping[j + 1] = jumping[j];
-//                j--;
-//            }
+        Console.Write("Десереализованные файлы: \n");
 
-//            jumping[j + 1] = key;
-//        }
+        file_name_1 = Path.Combine(path, file_name_1);
+        file_name_2 = Path.Combine(path, file_name_2);
 
-//        for (int i = 0; i < jumping.Length; i++)
-//        {
-//            Console.Write(jumping[i].GetRaceInfo(i + 1));
-//        }
-//    }
-//}
+        if (!File.Exists(file_name_1))
+        {
+            Json.Write<Task1>((Task1)tasks[0], file_name_1);
+            Json.Write<Task2>((Task2)tasks[1], file_name_2);
+        }
+        else
+        {
+            var t1 = Json.Read<Task1>(file_name_1);
+            var t2 = Json.Read<Task2>(file_name_2);
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+        }
+    }
+}
+
+
+
 
 
